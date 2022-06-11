50b7c550228315889d5452e761ca88463d98254b
Paula <paulasalinor@gmail.com>
2022-06-11 13:10:55 -0300

    feat: add middleware for validate age

---
+++ middlewares/validateAgeMiddleware.js
@@ -0,0 +1,16 @@
+const MIN_AGE = 18;
+
+const validateAgeMiddleware = (req, res, next) => {
+  const { age } = req.body;
+
+  if (!age) {
+    return res.status(400).json({ message: 'O campo "age" é obrigatório' });
+  }
+  if (age < MIN_AGE) {
+    return res.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+
+  next();
+};
+
+module.exports = validateAgeMiddleware;
